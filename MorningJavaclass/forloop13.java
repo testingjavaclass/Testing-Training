fdbc5ee6676d352a8efa55b79a5ee6ae8bfda38d
testingjavaclass <testingjavaclass@gmail.com>
2023-03-13 23:42:54 +0530

    13th march

--- MorningJavaclass/forloop13.java
+++ MorningJavaclass/forloop13.java
@@ -1,13 +1,13 @@
-class forloop12
+class forloop14
 {
 public static void main(String[] args)
 {
-for(int i=5;i>=1;i--)
+for(int i=1;i<=5;i++)
   {
 
-     for(int j=1;j<=i;j++)
+     for(int j=5;j>=i;j--)
        {
-        System.out.print(j);
+        System.out.print(i);
 
        }
 
--- MorningJavaclass/forloop15.java
+++ MorningJavaclass/forloop15.java
@@ -1,4 +1,4 @@
-class forloop12
+class forloop15
 {
 public static void main(String[] args)
 {
@@ -7,7 +7,7 @@ for(int i=5;i>=1;i--)
 
      for(int j=1;j<=i;j++)
        {
-        System.out.print(j);
+        System.out.print(i);
 
        }
 
