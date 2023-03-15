62ca9961180352b7ceaf6d3f70858300d0975362
testingjavaclass <testingjavaclass@gmail.com>
2023-03-15 19:40:59 +0530

    java program

---
+++ JAVAPROGRAMS/Program1.java
@@ -0,0 +1,11 @@
+package com.JAVAPROGRAMS;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Eclipse,This is my first program");
+		// TODO Auto-generated method stub
+
+	}
+
+}
